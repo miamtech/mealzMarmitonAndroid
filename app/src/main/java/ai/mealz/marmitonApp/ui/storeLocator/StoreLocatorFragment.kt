6a33d2d03754774a0e77922ae3df586cd9e55109
walerowicz-miam <damien.walerowicz@mealz.ai>
2024-04-18 15:24:15 +0200

    update store locator

--- app/src/main/java/ai/mealz/marmitonApp/ui/storeLocator/StoreLocatorFragment.kt
+++ app/src/main/java/ai/mealz/marmitonApp/ui/storeLocator/StoreLocatorFragment.kt
@@ -8,9 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ai.mealz.marmitonApp.databinding.FragmentStoreLocatorBinding
+import android.annotation.SuppressLint
 import android.widget.Button
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import org.json.JSONObject
 
-class StoreLocatorFragment : Fragment() {
+class StoreLocatorFragment() : Fragment() {
 
     private var _binding: FragmentStoreLocatorBinding? = null
 
@@ -18,12 +22,12 @@ class StoreLocatorFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @SuppressLint("JavascriptInterface")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentStoreLocatorBinding.inflate(inflater, container, false)
         val root: View = binding.root
         root.findViewById<Button>(R.id.button_store_A)
@@ -34,11 +38,36 @@ class StoreLocatorFragment : Fragment() {
             .setOnClickListener {
                 Mealz.user.setStoreId("25910")
             }
+
+        val myWebView: WebView = root.findViewById(R.id.webview)
+        val webViewSettings = myWebView.settings;
+        webViewSettings.javaScriptEnabled = true
+        webViewSettings.allowFileAccessFromFileURLs = true
+        webViewSettings.allowUniversalAccessFromFileURLs = true
+        webViewSettings.allowFileAccess = true
+        webViewSettings.allowContentAccess = true
+        Companion.onSelectStore?.let { JsObjcet(it) }?.let { myWebView.addJavascriptInterface(it, "Mealz") }
+        myWebView.loadUrl("file:///android_asset/index.html");
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        var onSelectStore: ((String) -> Void)? = null
+    }
+}
+
+class JsObjcet(var onSelectStore: (String) -> Void) {
+    @JavascriptInterface
+    fun postMessage(message: String) {
+        println(message)
+        val storeId = JSONObject(message).get("id") as String
+        Mealz.user.setStoreId(storeId)
+        onSelectStore(storeId)
     }
 }
