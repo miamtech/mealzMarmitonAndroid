86fa4e66ad4bef6ca3730f83cb6b7e7d04de15e1
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2024-04-30 09:09:45 +0100

    add nav func to item selector from basket journey

--- marmiton/src/main/java/ai/mealz/marmiton/config/components/MyBasketJourney/MyBasketJourney.kt
+++ marmiton/src/main/java/ai/mealz/marmiton/config/components/MyBasketJourney/MyBasketJourney.kt
@@ -1,8 +1,11 @@
 package ai.mealz.marmiton.config.components.MyBasketJourney
 
+import ai.mealz.core.viewModels.itemSelector.ItemSelectorContract
+import ai.mealz.core.viewModels.itemSelector.ItemSelectorViewModel
 import ai.mealz.core.viewModels.myBasket.MyBasketViewModel
 import ai.mealz.core.viewModels.myMeal.MyMealViewModel
 import ai.mealz.core.viewModels.myProducts.MyProductsViewModel
+import ai.mealz.sdk.components.itemSelector.ItemSelector
 import ai.mealz.sdk.components.myBasket.MyBasket
 import android.content.Context
 import android.util.AttributeSet
@@ -12,6 +15,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.AbstractComposeView
 import androidx.compose.ui.viewinterop.AndroidView
@@ -28,27 +32,30 @@ class MyBasketJourney @JvmOverloads constructor(
 ): AbstractComposeView(context, attrs, defStyleAttr) {
 
     private val myBasketVm = MyBasketViewModel()
-    private val myProductsVm = MyProductsViewModel(openItemSelector = {})
     private val myMealsVm = MyMealViewModel()
+    private val itemSelectorViewModel = ItemSelectorViewModel()
+
     @Composable
     override fun Content() {
         val navController = rememberNavController()
-        fun goToTransferBasket(url: String)
-            {
-                val encodedUrl: String = URLEncoder.encode(url, StandardCharsets.UTF_8.toString())
-                navController.navigate("TRANSFER_BASKET/${encodedUrl}") { launchSingleTop = true }
-            }
+        fun goToTransferBasket(url: String) {
+            val encodedUrl: String = URLEncoder.encode(url, StandardCharsets.UTF_8.toString())
+            navController.navigate("TRANSFER_BASKET/${encodedUrl}") { launchSingleTop = true }
+        }
+        fun goToItemSelector() {
+            navController.navigate("ITEM_SELECTOR") { launchSingleTop = true }
+        }
 
         NavHost(navController = navController, startDestination = "MY_BASKET") {
             composable("MY_BASKET") {
               MyBasket.View(
                   myBasketVm = myBasketVm,
                   myMealsVm = myMealsVm,
-                  myProductsVm = myProductsVm,
                   onButtonClickedWhenEmpty = {},
                   onTransferBasketNavigation = {
                       goToTransferBasket(it)
-                  })
+                  },
+                  onNavigateToItemSelector = { goToItemSelector() })
             }
             composable("TRANSFER_BASKET/{url}"){ backStackEntry ->
                 backStackEntry.arguments?.let { arguments ->
@@ -65,6 +72,15 @@ class MyBasketJourney @JvmOverloads constructor(
                             })
                         }
                     }
+                }
+            }
+            composable("ITEM_SELECTOR") { backStackEntry ->
+                ItemSelector.View(
+                    itemSelectorViewModel,
+                    null
+                ) {
+                    itemSelectorViewModel.setEvent(ItemSelectorContract.Event.ReturnToDetail)
+                    navController.navigate("MY_BASKET") { launchSingleTop = false }
                 }
             }
         }
