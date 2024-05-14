7b7476071eecdf63d44a776f6d043d672adfb122
diarmuidmcgonagle <diarmuidmcgonagle@gmail.com>
2024-05-14 11:36:48 +0200

    add analytics events to store locator

--- app/src/main/java/ai/mealz/marmitonApp/ui/storeLocator/StoreLocatorFragment.kt
+++ app/src/main/java/ai/mealz/marmitonApp/ui/storeLocator/StoreLocatorFragment.kt
@@ -1,24 +1,15 @@
 package ai.mealz.marmitonApp.ui.storeLocator
 
-import ai.mealz.core.Mealz
-import ai.mealz.marmiton.config.components.webview.MealzWebView
+import ai.mealz.marmiton.config.components.webview.MealzStoreLocatorWebView
 import ai.mealz.marmitonApp.R
 import ai.mealz.marmitonApp.databinding.FragmentStoreLocatorBinding
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.JavascriptInterface
-import android.webkit.WebView
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 
 
@@ -40,7 +31,7 @@ class StoreLocatorFragment() : Fragment() {
         _binding = FragmentStoreLocatorBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val myWebView: MealzWebView = root.findViewById(R.id.store_locator)
+        val myWebView: MealzStoreLocatorWebView = root.findViewById(R.id.store_locator)
         myWebView.urlToLoad = "file:///android_asset/index.html"
         myWebView.onSelectStore = { _ ->
             val mainHandler = context?.mainLooper?.let { Handler(it) }
