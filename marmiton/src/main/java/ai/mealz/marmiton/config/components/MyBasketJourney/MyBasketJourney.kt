916f32a88fc4e848893d4e5898b632c400f1e620
Antonin Francois <antonin.francois@mealz.ai>
2024-08-23 15:22:27 +0200

    Fix basket transfer webview

--- marmiton/src/main/java/ai/mealz/marmiton/config/components/MyBasketJourney/MyBasketJourney.kt
+++ marmiton/src/main/java/ai/mealz/marmiton/config/components/MyBasketJourney/MyBasketJourney.kt
@@ -4,23 +4,13 @@ import ai.mealz.core.viewModels.itemSelector.ItemSelectorContract
 import ai.mealz.core.viewModels.itemSelector.ItemSelectorViewModel
 import ai.mealz.core.viewModels.myBasket.MyBasketViewModel
 import ai.mealz.core.viewModels.myMeal.MyMealViewModel
-import ai.mealz.core.viewModels.myProducts.MyProductsViewModel
 import ai.mealz.sdk.components.itemSelector.ItemSelector
 import ai.mealz.sdk.components.myBasket.MyBasket
+import ai.mealz.sdk.components.transferBasket.TransferBasket
 import android.content.Context
 import android.util.AttributeSet
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.AbstractComposeView
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -62,33 +52,8 @@ class MyBasketJourney @JvmOverloads constructor(
             }
             composable("TRANSFER_BASKET/{url}"){ backStackEntry ->
                 backStackEntry.arguments?.let { arguments ->
-                    Column {
-                        Box(modifier = Modifier.fillMaxSize()) {
-                            AndroidView(factory = {
-                                WebView(it).apply {
-                                    this.webViewClient = object : WebViewClient() {
-                                        override fun shouldOverrideUrlLoading(
-                                            view: WebView,
-                                            request: WebResourceRequest
-                                        ): Boolean {
-                                            view.loadUrl(request.url.toString())
-                                            return false
-                                        }
-                                    }
-                                    this.settings.javaScriptEnabled = true
-                                    this.settings.allowFileAccessFromFileURLs = true
-                                    this.settings.allowUniversalAccessFromFileURLs = true
-                                    this.settings.allowFileAccess = true
-                                    this.settings.allowContentAccess = true
-                                    this.settings.setDomStorageEnabled(true);
-                                    layoutParams = LayoutParams(
-                                        LayoutParams.MATCH_PARENT,
-                                        LayoutParams.MATCH_PARENT
-                                    )
-                                    this.loadUrl(arguments.getString("url") ?: "")
-                                }
-                            })
-                        }
+                    TransferBasket.View(url = arguments.getString("url") ?: "", retailerName = "toto") {
+
                     }
                 }
             }
